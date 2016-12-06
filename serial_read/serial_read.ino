db586044cedfe4f835394ad69a3b009ef208ead2
James Neumann <jrneumann1@gmail.com>
2016-12-05 20:22:41 -0600

    Updating serial interfacing

--- serial_read/serial_read.ino
+++ serial_read/serial_read.ino
@@ -1,23 +1,23 @@
 void setup() {
-        Serial.begin(9600);     // set 9600 bps data rate
-        pinMode(LED_BUILTIN, OUTPUT); // set onboard LED
+    Serial.begin(9600);     // set 9600 bps data rate
+    pinMode(13, OUTPUT); // set onboard LED
 }
 
 void loop() {
 
-        int userInput = 1;   // for incoming serial data
+    int userInput = 1;   // for incoming serial data
 
-        if (Serial.available() > 0) {
-                userInput = Serial.read(); // read input data
+    if (Serial.available() > 0) {
+        userInput = Serial.read(); // read input data
         
-          if (userInput == 49) {
-            digitalWrite(LED_BUILTIN, HIGH); // Turn on when "1" is read
-            }   
-          else if (userInput == 48) {
-            digitalWrite(LED_BUILTIN, LOW); // Turn off when "0" is read
-            }
-          else {
-            digitalWrite(LED_BUILTIN, LOW);  
-            }
-       }
+      if (userInput == 49) {
+          digitalWrite(13, HIGH); // Turn on when "1" is read
+      }   
+      else if (userInput == 48) {
+          digitalWrite(13, LOW); // Turn off when "0" is read
+      }
+      else {
+          digitalWrite(LED_BUILTIN, LOW);  
+      }
+    }
 }
